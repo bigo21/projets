fb0fc695c5ef93ee225a25494ceb0642c0be62d6
bigo21 <loicango11@gmail.com>
2024-10-07 13:45:27 +0100

    "Début de modification du ccode"

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footer = () => {
+    return (
+        <footer>
+            App Footer
+        </footer>
+    )
+}
+
+export default footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header>
+            App Header 
+        </header>
+    )
+}
+
+export default header
